215bdbc7686507637a75100658cfd65ddb41f694
VAlmea <almeavicente@gmail.com>
2020-11-27 08:28:29 -0500

    geolocation

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,49 @@
 <template>
   <v-app class="v-flex">
-   <component :is="layout">
-     <router-view :layout.sync="layout"></router-view>
-   </component>   
+    <component :is="layout">
+      <router-view :layout.sync="layout"></router-view>
+    </component>
   </v-app>
 </template>
 
 <script>
-import Principal from './components/Layouts/PrincipalLayout';
-import Login from './components/Layouts/LoginLayout';
-import Admin from './components/Layouts/AdminLayout';
+import Principal from "./components/Layouts/PrincipalLayout";
+import Login from "./components/Layouts/LoginLayout";
+import Admin from "./components/Layouts/AdminLayout";
 export default {
-  components:{
+  components: {
     Principal,
     Login,
-    Admin
+    Admin,
   },
-  name: 'App',
+  name: "App",
   data: () => ({
-    layout:'Principal'
+    layout: "Principal",
+    location: null,
+    gettingLocation: false,
+    errorStr: null,
   }),
+  created() {
+            console.log("Hola")
+
+    //do we support geolocation
+    if (!("geolocation" in navigator)) {
+      this.errorStr = "Geolocation is not available.";
+      return;
+    }
+
+    this.gettingLocation = true;
+    // get position
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        this.gettingLocation = false;
+        this.location = pos;
+      },
+      (err) => {
+        this.gettingLocation = false;
+        this.errorStr = err.message;
+      },
+    );
+  },
 };
 </script>
